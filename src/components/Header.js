38ace47b9f82e30a71b88635f8f58809328d8fe1
Jacob Kwan <77665182+jacobkwan@users.noreply.github.com>
2021-07-06 23:31:13 +0800

    create Button comp and use in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,31 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+
 const Header = ({ title }) => {
+  const onClick = (e) => {
+    console.log(e.target)
+  }
+
   return (
-    <header>
+    <header className='header'>
       <h1>{title}</h1>
+      <Button color='green' text='Add' onClick={onClick} />
     </header>
   )
 }
 
 Header.defaultProps = {
-  title: 'Task Tracker'
+  title: "Task Tracker"
 }
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+}
+
+// CSS in JS, use it e.g <h1 style = {headingStyle}>
+// const headingStyle = {
+//   color: 'red',
+//   backgroundColor: 'black'
+// }
 
 export default Header
