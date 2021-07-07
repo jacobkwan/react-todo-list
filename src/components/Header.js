ec64eaeb875b9a6d8bafb30fa7127ffe3760915c
Jacob Kwan <77665182+jacobkwan@users.noreply.github.com>
2021-07-07 23:20:20 +0800

    eslint clean up

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,26 @@
-import PropTypes from 'prop-types'
-import Button from './Button'
+import PropTypes from "prop-types";
+import Button from "./Button";
 
 const Header = ({ title }) => {
   const onClick = (e) => {
-    console.log(e.target)
-  }
+    console.log(e.target);
+  };
 
   return (
-    <header className='header'>
+    <header className="header">
       <h1>{title}</h1>
-      <Button color='green' text='Add' onClick={onClick} />
+      <Button color="green" text="Add" onClick={onClick} />
     </header>
-  )
-}
+  );
+};
 
 Header.defaultProps = {
-  title: "Task Tracker"
-}
+  title: "Task Tracker",
+};
 
 Header.propTypes = {
   title: PropTypes.string.isRequired,
-}
+};
 
 // CSS in JS, use it e.g <h1 style = {headingStyle}>
 // const headingStyle = {
@@ -28,4 +28,4 @@ Header.propTypes = {
 //   backgroundColor: 'black'
 // }
 
-export default Header
+export default Header;
